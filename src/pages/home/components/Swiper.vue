39070f5246586a72156268844a2186da1acd6f9c
Dhr_Brick <dengheran@aliyun.com>
2018-09-15 10:01:46 +0800

    add swiper

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>轮播</div>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" >    </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeSwiper'
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        'id': '0001',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+      }, {
+        'id': '0002',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+      }, {
+        'id': '0003',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+      }, {
+        'id': '0004',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
+      }]
+    }
+  }
 }
 </script>
 
-<style></style>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 31.25%
+    background-color: #eee
+    .swiper-img
+      width: 100%
+</style>
