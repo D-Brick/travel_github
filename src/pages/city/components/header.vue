588fbaee72e03a64e5a5b2640e683e66b7d2f227
Dhr_Brick <dengheran@aliyun.com>
2018-09-18 14:11:48 +0800

    add header

---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+    城市选择
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    .back-icon
+      position: absolute
+      left: .12rem
+      font-size: .4rem
+      color: #fff
+</style>
